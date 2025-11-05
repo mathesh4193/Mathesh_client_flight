08cb5839bdb9e2d60f49a5e1017c165cc2c443c8
Mathesh M <mathesh4193@gmail.com>
2025-11-05 20:06:32 +0530

    Commit

--- src/config/api.js
+++ src/config/api.js
@@ -1,1 +1,3 @@
-export const API_BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:5000"||"https://mathesh-fligh-server.onrender.com";
+export const API_BASE_URL =
+  process.env.REACT_APP_API_URL ||
+  "https://mathesh-fligh-server.onrender.com";
